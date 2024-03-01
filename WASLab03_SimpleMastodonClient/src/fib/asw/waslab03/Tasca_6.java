a5d9805287da92889cb1ef325876c6d7e137e80f
Javiergc168 <javiergc168@gmail.com>
2024-03-01 11:55:30 +0100

    unfinished #6

--- WASLab03_SimpleMastodonClient/src/fib/asw/waslab03/Tasca_6.java
+++ WASLab03_SimpleMastodonClient/src/fib/asw/waslab03/Tasca_6.java
@@ -1,10 +1,34 @@
 package fib.asw.waslab03;
 
+import org.apache.hc.client5.http.fluent.Request;
+import org.json.JSONArray;
+
 public class Tasca_6 {
 
 	public static void main(String[] args) {
+		String URI = "https://mastodont.cat/api/v1/trends/tags";
+		String TOKEN = Token.get();
+		
+		try {
+			String output = Request.get(URI)
+					.addHeader("Authorization","Bearer "+TOKEN)
+					.execute()
+					.returnContent()
+					.asString();
+			//System.out.format(output);
+			JSONArray result = new JSONArray(output);
+			for (int i=0; i<10; i++) {
+				String tag = result.getJSONObject(i).getString("name");
+				String URI2 = "https://mastodont.cat/api/v1/trends/tags";
+			}
 
-		System.out.println("Encara no faig res!");
+			
+
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
 	}
 
 }
