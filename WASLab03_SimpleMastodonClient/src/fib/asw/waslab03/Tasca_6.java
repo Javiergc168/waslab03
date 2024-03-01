782702eadc2945721efc32a03fb561d44bff6f8a
alvar <alvar@AsusAlvar>
2024-03-01 18:00:26 +0100

    task #6 almost finished

--- WASLab03_SimpleMastodonClient/src/fib/asw/waslab03/Tasca_6.java
+++ WASLab03_SimpleMastodonClient/src/fib/asw/waslab03/Tasca_6.java
@@ -6,7 +6,7 @@ import org.json.JSONArray;
 public class Tasca_6 {
 
 	public static void main(String[] args) {
-		String URI = "https://mastodont.cat/api/v1/trends/tags";
+		String URI = "https://mastodont.cat/api/v1/trends/tags"; // limit 10
 		String TOKEN = Token.get();
 		
 		try {
@@ -15,15 +15,28 @@ public class Tasca_6 {
 					.execute()
 					.returnContent()
 					.asString();
-			//System.out.format(output);
+			System.out.format("Els 10 tags més populars a Mastodon [" + ""/*data i hora actual*/ + "]");
 			JSONArray result = new JSONArray(output);
 			for (int i=0; i<10; i++) {
 				String tag = result.getJSONObject(i).getString("name");
-				String URI2 = "https://mastodont.cat/api/v1/trends/tags";
+				System.out.format("*************************************************");
+				System.out.format("* Tag: " + tag);
+				System.out.format("*************************************************");
+
+				String URI2 = "https://mastodont.cat/api/v1/timelines/tag/" + tag;	//limit 5			
+				String output2 = Request.get(URI2)
+						.addHeader("Authorization","Bearer "+TOKEN)
+						.execute()
+						.returnContent()
+						.asString();
+				JSONArray result2 = new JSONArray(output2);
+				for (int j=0; j<5; j++) {
+					String content = result2.getJSONObject(i).getString("content");
+					System.out.format("- " + content);	
+					System.out.format("-------------------------------------------------");
+				}
+				System.out.format("\n");	
 			}
-
-			
-
 		}
 		catch (Exception ex) {
 			ex.printStackTrace();
